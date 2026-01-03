f300382d39e339202c1ea5899af4c8737c22a602
Amit Patidar <amit.patidar6264@gmail.com>
2026-01-03 22:57:10 +0530

    day-5

---
+++ Day-5-remote-backend-dynamodb-lock/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "amit-patidar-12321"
+    key    = "terraform.tfstate"
+    region = "eu-north-1"
+   
+    # Enable S3 native locking
+    #use_lockfile = true   #tf version should be above 1.10
+    # The dynamodb_table argument is no longer needed
+    dynamodb_table = "terraform-state-lock-dynamo"
+
+  }
+}
